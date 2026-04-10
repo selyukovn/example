ba239d8cfc75d0f2b499e37acf12fccbfe41cb36
Nikita Selyukov <selyukovn@yandex.ru>
2026-04-10 20:56:50 +0300

    Kafka. Совмещены роли брокера и контроллера для упрощения кластера (3 ноды).

--- infrastructure/kafka/build/server/healthcheck.sh
+++ infrastructure/kafka/build/server/healthcheck.sh
@@ -6,8 +6,9 @@ vType=${KAFKA_PROCESS_ROLES}
 if [ "$vType" = "broker" ]; then
   kafka-metadata-quorum --bootstrap-server=localhost:9092 describe --status > /dev/null
 elif [ "$vType" = "controller" ]; then
-  kafka-metadata-quorum --bootstrap-controller=localhost:9092 describe --status > /dev/null
+  kafka-metadata-quorum --bootstrap-controller=localhost:9093 describe --status > /dev/null
 else
-  echo "ОШИБКА! HEALTHCHECK не определен для узла комбинированного типа!"
-  exit 1
+  # broker + controller
+  kafka-metadata-quorum --bootstrap-server=localhost:9092 describe --status > /dev/null
+  kafka-metadata-quorum --bootstrap-controller=localhost:9093 describe --status > /dev/null
 fi
