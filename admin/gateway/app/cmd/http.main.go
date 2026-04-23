23a01bff5e2370bc9d91bdedd034e1b0b9280f62
Nikita Selyukov <selyukovn@yandex.ru>
2026-04-23 14:07:40 +0300

    Рефакторинг. В `Gateway` container.New() не возвращает тикер in-memory кеша, поскольку подобные тикеры не нуждается в graceful-shutdown.
    Точка запуска подобных тикеров может быть любой, в т.ч. неявной.

--- admin/gateway/app/cmd/http.main.go
+++ admin/gateway/app/cmd/http.main.go
@@ -55,7 +55,7 @@ func main() {
 	// Container
 	// -----------------------------------------------------------------------------------------------------------------
 
-	ctr, authCacheTicker := container.New(
+	ctr := container.New(
 		assert.Str().NotEmpty().MustGet(os.Getenv("SERVICE_AUTH_API_GRPC_BASEURL"), "env: SERVICE_AUTH_API_GRPC_BASEURL"),
 		assert.Str().NotEmpty().MustGet(os.Getenv("SERVICE_AUTH_API_GRPC_APIKEY"), "env: SERVICE_AUTH_API_GRPC_APIKEY"),
 	)
@@ -77,11 +77,6 @@ func main() {
 			"Monitoring-сервер",
 			func(context.Context) error { return monServer.Start() },
 			func(context.Context) error { return monServer.Stop() },
-		},
-		{
-			"Auth-Cache-Ticker",
-			func(context.Context) error { authCacheTicker.Start(); return nil },
-			func(context.Context) error { authCacheTicker.Stop(); return nil },
 		},
 	})
 
--- admin/gateway/app/cmd/http/container/container.go
+++ admin/gateway/app/cmd/http/container/container.go
@@ -10,6 +10,9 @@ import (
 	infra_cache_memory "example/admin/gateway/internal/infra/cache/memory"
 	infra_clients_auth "example/admin/gateway/internal/infra/clients/auth"
 	infra_clients_auth_grpc "example/admin/gateway/internal/infra/clients/auth/grpc"
+	"fmt"
+	goroutiner "github.com/selyukovn/go-routiner"
+	"github.com/selyukovn/go-std/logger"
 	assert "github.com/selyukovn/go-wm-assert"
 )
 
@@ -24,18 +27,13 @@ type Services = struct {
 func New(
 	appCfmApiGrpcBaseUrl string,
 	appCfmApiGrpcApiKey string,
-) (
-	// TODO : РЕФАКТОРИНГ : мемори-кеш требует запуска тикера, который нужно уметь аккуратно останавливать,
-	// 		но это никак не должно быть связано с контейнером.
-	*Container,
-	infra_cache_memory.Ticker,
-) {
+) *Container {
 	assert.Str().NotEmpty().Must(appCfmApiGrpcBaseUrl)
 	assert.Str().NotEmpty().Must(appCfmApiGrpcApiKey)
 
 	// -----------------------------------------------------------------------------------------------------------------
 
-	// auth
+	// <auth-service>
 	var sAuth infra_clients_auth.ClientInterface
 	sAuth = infra_clients_auth_grpc.NewClientGrpcMust(
 		appCfmApiGrpcBaseUrl,
@@ -44,14 +42,25 @@ func New(
 			return processing.OperationId(ctx)
 		},
 	)
+	// +cache
 	var authCache infra_cache.CacheInterface
 	authCache, authCacheTicker := infra_cache_memory.New()
+	// Точка запуска тикера может быть любой, поскольку ему не требуется graceful-shutdown.
+	goroutiner.New(goroutiner.MwPanicToError(func(pv any, ds []byte, ctx context.Context) error {
+		logger.PanicFf(ctx, pv, ds, "goroutiner.authCacheTicker")
+		return fmt.Errorf("panic: %#v; stack: %s", pv, ds)
+	})).SingleAsync(context.Background(), func(ctx context.Context) error {
+		authCacheTicker.Start()
+		return nil
+	})
 	authCache = adapt_infra_cache.NewDecoratorLoggable(authCache, true)
 	sAuth = adapt_infra_clients_auth.NewDecoratorCacheable(
 		sAuth,
 		adapt_infra_clients_auth_cacher.NewCacher(authCache),
 	)
+	// +logger
 	sAuth = adapt_infra_clients_auth.NewDecoratorLoggable(sAuth)
+	// </auth-service>
 
 	// -----------------------------------------------------------------------------------------------------------------
 
@@ -61,5 +70,5 @@ func New(
 		Services: Services{
 			Auth: sAuth,
 		},
-	}, authCacheTicker
+	}
 }
