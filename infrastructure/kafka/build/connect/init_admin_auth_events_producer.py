4c85322bac8216066b2912d1e3a815eb491b0fc5
Nikita Selyukov <selyukovn@yandex.ru>
2026-04-28 21:08:05 +0300

    `Auth` публикует события в `admin-auth-events` топик с помощью `kafka-connect`.

---
+++ infrastructure/kafka/build/connect/init_admin_auth_events_producer.py
@@ -0,0 +1,153 @@
+import urllib.request
+import urllib.parse
+import json
+import os
+
+# https://habr.com/ru/companies/deliveryclub/articles/529484/
+# https://debezium.io/documentation/reference/3.5/connectors/mysql.html
+# https://docs.confluent.io/platform/current/connect/references/restapi.html#connectors
+
+# У Debezium, как оказалось, есть специальный трансформатор под outbox-паттерн,
+# но узнал я об этом уже после того, как настроил этот коннектор.
+# https://debezium.io/documentation/reference/3.5/transformations/outbox-event-router.html
+# Переделывать не стал, т.к. outbox-трансформатор накладывает ограничения на схему хранения и другие трансформаторы.
+# Потому этот коннектор остается, каким он и был, а на outbox'е, возможно, будет другой -- для разнообразия.
+
+# --
+
+allBrokersString = os.environ["CONNECT_BOOTSTRAP_SERVERS"]
+brokersCount = max(1, len(allBrokersString.split(",")))
+# Количеству партиций желательно быть кратным количеству брокеров для равномерного распределения.
+# Количеству партиций желательно быть > 1 для предоставления потребителям возможности распараллеливать обработку.
+# Количеству партиций желательно быть не слишком большим без необходимости для экономии ресурсов.
+# Не ожидается, что admin-auth-сервис будет генерировать много событий, поэтому достаточно небольшого кол-ва партиций.
+publicTopicPartitionsCount = min(3, brokersCount)
+
+# Внимание!
+# Debezium по умолчанию предоставляет гарантию `at-least-once` (дубликаты возможны при восстановлении после сбоев).
+# Некоторые коннекторы поддерживают гарантию `exactly-once`, но нет ни необходимости, ни желания ее настраивать.
+# https://debezium.io/documentation/reference/3.5/connectors/mysql.html#mysql-when-things-go-wrong
+# https://debezium.io/documentation/reference/3.5/configuration/eos.html
+
+# --
+
+connectorName = "mysql-admin-auth-event-producer"
+dbTables = "auth.event"
+topicRedirectFromRegexp = "mysql-admin-auth-event-producer.auth.event"
+topicRedirectTo = "admin-auth-events"
+partitionKeyingFields = "change.outbox_group_id"
+
+# --
+
+req = urllib.request.Request(
+    "http://localhost:8083/connectors",
+    method="POST",
+    headers={"Content-Type": "application/json"},
+    data=json.dumps({
+        "name": connectorName,
+        "config": {
+            "connector.class": "io.debezium.connector.mysql.MySqlConnector",
+
+            # ---- mysql ----
+            "database.server.id": 100501,
+            "database.hostname": os.environ["MYSQL_HOST"],
+            "database.port": os.environ["MYSQL_PORT"],
+            "database.user": os.environ["MYSQL_USER"],
+            "database.password": os.environ["MYSQL_PASSWORD"],
+            "table.include.list": dbTables,
+            # ---- /mysql ----
+
+            # ---- schema.history ----
+            # databases -- однозначно true, т.к. в теории разные базы -- это разные сервера, а значит и коннекторы.
+            # tables -- коннектор играет роль продюсера событий, поэтому использование других таблиц не ожидается.
+            "schema.history.internal.store.only.captured.databases.ddl": True,
+            "schema.history.internal.store.only.captured.tables.ddl": True,
+            # ---- /schema.history ----
+
+            # ---- topics ----
+            "schema.history.internal.kafka.bootstrap.servers": allBrokersString,
+
+            # По умолчанию topic.creation.enable = true, поэтому будут созданы топики:
+            #
+            # - `connectorName` с DDL:
+            #   SET character_set_server=utf8mb4, collation_server=utf8mb4_0900_ai_ci
+            #   DROP TABLE IF EXISTS `auth`.`event`
+            #   DROP DATABASE IF EXISTS `auth`
+            #   CREATE DATABASE `auth` CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci
+            #   USE `auth`
+            #   CREATE TABLE `event` ...
+            #
+            # - технический топик для внутренних дел -- `connectorName + "-history"`:
+            #   SET character_set_server=utf8mb4, collation_server=utf8mb4_0900_ai_ci
+            #   DROP TABLE IF EXISTS `auth`.`event`
+            #   CREATE TABLE `event` ...
+            #
+            # - по каждой табличке из dbTables -- `connectorName + "." + {dbName.tableName}`:
+            #   ... данные ...
+            "schema.history.internal.kafka.topic": connectorName + "-history",
+            "topic.prefix": connectorName,
+
+            # https://debezium.io/documentation/reference/3.5/configuration/topic-auto-create-config.html
+            # По умолчанию топики будут созданы только с 1 партицией без реплик.
+            # В топике `connectorName` будет DDL -- он не нужен для продюсирования событий.
+            # Но для топика с данными это снижает надежность и лишает возможности параллельно обрабатывать события,
+            # т.е. в рамках одного сервиса-приемника может быть запущен только один консьюмер на группу
+            # (по факту больше может быть и не нужно, но перспектива дальнейшего развития должна быть открыта).
+            #
+            # Внимание!
+            # Использование нескольких партиций обязует таблицы предоставлять ключ партицирования,
+            # по которому будет вычисляться конкретная партиция, в которую будет отправлено конкретное событие.
+            # Это необходимо для того, чтобы события из определенных групп (например, по пользователю)
+            # принимались в том же порядке, в котором они были записаны в таблицу событий,
+            # а также чтобы события из разных групп могли быть обработаны параллельно.
+            # Технически можно собрать такой ключ прямо здесь (см. документацию), но это место легко забыть,
+            # а последствия непоследовательной обработки событий тихо распространятся на всю систему.
+            # Поэтому лучше иметь специальную колонку в таблице событий, которая будет видна при внесении изменений.
+            # Таблица событий ведется именно для их распространения, поэтому никакие зависимости не будут нарушены.
+            #
+            # Суть `event-producer`-коннектора в использовании одной таблички
+            # (или нескольких табличек-частей логически целой таблицы, например, events_2025, events_2026, ...),
+            # поэтому достаточно одной группы параметров создания топиков,
+            # тем более в данном случае из-за "редиректа" все события перенаправляются в единственный топик.
+            # Однако, debezium требует обязательно указать `replication.factor` и `partitions` для группы `default`.
+            # Топик `connectorName` с DDL будет создан по default-группе, поскольку не войдет в вышеописанную.
+            "topic.creation.default.replication.factor": brokersCount,
+            "topic.creation.default.partitions": 1,
+            "topic.creation.groups": topicRedirectTo,
+            "topic.creation."+topicRedirectTo+".replication.factor": brokersCount,
+            "topic.creation."+topicRedirectTo+".partitions": publicTopicPartitionsCount,
+            "topic.creation."+topicRedirectTo+".include": topicRedirectTo,
+
+            # Для наглядности, сокрытия реализации продюсера и эксперимента ради используется "публичный" топик.
+            # Он будет создан вместо `connectorName + ".dbName.tableName"` топика(-ов?).
+            # https://kafka.apache.org/42/kafka-connect/user-guide/#org.apache.kafka.connect.transforms.RegexRouter
+            "transforms.RedirectToPublicTopic.type": "org.apache.kafka.connect.transforms.RegexRouter",
+            "transforms.RedirectToPublicTopic.regex": topicRedirectFromRegexp,
+            "transforms.RedirectToPublicTopic.replacement": topicRedirectTo,
+
+            # https://debezium.io/documentation/reference/3.5/transformations/partition-routing.html
+            "transforms.Partitioning.type": "io.debezium.transforms.partitions.PartitionRouting",
+            "transforms.Partitioning.partition.payload.fields": partitionKeyingFields,
+            "transforms.Partitioning.partition.topic.num": publicTopicPartitionsCount,
+            "transforms.Partitioning.predicate": topicRedirectTo,
+            "predicates."+topicRedirectTo+".type": "org.apache.kafka.connect.transforms.predicates.TopicNameMatches",
+            "predicates."+topicRedirectTo+".pattern": topicRedirectTo,
+            # ---- /topics ----
+
+            "transforms": "RedirectToPublicTopic,Partitioning",
+            "predicates": topicRedirectTo,
+        }
+    }).encode("utf-8"),
+)
+
+try:
+    with urllib.request.urlopen(req) as response:
+        result = json.loads(response.read().decode("utf-8"))
+        print("Статус:", response.getcode())
+        print("Ответ:", result)
+except urllib.error.HTTPError as e:
+    error_details = e.read().decode("utf-8")
+    print(f"Status Code: {e.code}")
+    print(f"Reason: {e.reason}")
+    print(f"Body: {error_details}")
+    raise e
